02c3554098f9b1ef363fe3271dd02e2a7d794ecb
mhuerster <mary.huerster@gmail.com>
2015-03-20 17:46:19 -0700

    use pointers instead of shift for merge

--- sorting/merge_sort.rb
+++ sorting/merge_sort.rb
@@ -1,27 +1,33 @@
 def merge_sort(arr)
   length = arr.length
-  midpoint = length/2
-
+  
   return arr if length < 2
 
-  left_half = arr[0..midpoint-1]
-  right_half = arr[midpoint..length]
-
-  merge(merge_sort(left_half), merge_sort(right_half))
-
+  left_arr = arr[0...length/2]
+  right_arr = arr[length/2...length]
+  
+  merge(merge_sort(left_arr), merge_sort(right_arr))
 end
 
-def merge(left_arr, right_arr)
+def merge(arr1, arr2)
+
   merged_arr = []
-  until left_arr.empty? && right_arr.empty?
-    if left_arr.empty?
-      merged_arr << right_arr.shift
-    elsif right_arr.empty?
-      merged_arr << left_arr.shift
-    elsif left_arr[0] <= right_arr[0]
-      merged_arr << left_arr.shift
-    elsif right_arr[0] < left_arr[0]
-      merged_arr << right_arr.shift
+  pointer1 = 0
+  pointer2 = 0
+
+  until merged_arr.length == arr1.length + arr2.length
+    if arr1[pointer1].nil?
+      merged_arr << arr2[pointer2]
+      pointer2 += 1
+    elsif  arr2[pointer2].nil?
+      merged_arr << arr1[pointer1]
+      pointer1 += 1
+    elsif arr1[pointer1] >= arr2[pointer2]
+      merged_arr << arr2[pointer2]
+      pointer2 += 1
+    elsif arr2[pointer2] > arr1[pointer1]
+      merged_arr << arr1[pointer1]
+      pointer1 += 1
     end
   end
   merged_arr
