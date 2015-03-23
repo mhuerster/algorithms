387c0a7cbc6db62443edefb90f623d92851e8a00
mhuerster <mary.huerster@gmail.com>
2015-03-22 17:13:24 -0700

    advantages and disadvantages (TODO: flesh out)

---
+++ data-structures/linked-list.rb
@@ -0,0 +1,9 @@
+# ADVANTAGES
+
+# lookup first and last node in constant time
+# insert and delete at first and last node in constant time
+# good base for stack/queue implementation
+
+# DISADVANTAGES
+
+# lookup intermediate nodes in O(n)
