3b76d234335fa13307ad7cf02fe393c6bf37a26b
mhuerster <mary.huerster@gmail.com>
2015-03-27 22:59:09 -0700

    check edge for exactly two nodes

--- spec/data-structures/graph.rb
+++ spec/data-structures/graph.rb
@@ -2,9 +2,11 @@ require 'rspec'
 require_relative '../../data-structures/graph'
 
 describe 'graph' do
+
   before(:each) do
       @graph = Graph.new
     end
+  
   it 'is initialized empty' do
     expect(@graph.nodes).to be_empty
     expect(@graph.edges).to be_empty
@@ -25,10 +27,15 @@ describe 'graph' do
     expect(@graph.edges.length).to eq(1)
   end
 
-  it 'cannot have an edge without at least two nodes' do
+  it 'cannot have an edge without exactly two nodes' do
     node1 = Node.new(1)
+    node2 = Node.new(2)
+    node3 = Node.new(3)
     @graph.add_node(node1)
+    @graph.add_node(node2)
+    @graph.add_node(node3)
     expect { @graph.add_edge(node1) }.to raise_error(ArgumentError)
+    expect { @graph.add_edge(node1, node2, node3) }.to raise_error(ArgumentError)
   end
 
   it 'cannot add an edge unless both nodes exist in the graph' do
